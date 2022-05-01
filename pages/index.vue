a834add3e7558d17f0d65d0021edbf24cec25ea5
Ean <ean@eankrenzin.com>
2022-04-30 21:07:13 -0700

    add loading

--- pages/index.vue
+++ pages/index.vue
@@ -139,7 +139,17 @@ export default {
     </div>
   </div>
 
-  <div v-if="loading">loading</div>
+  <div v-if="loading">
+    
+<div class="loadingContainer">
+  <div class="lds-ellipsis">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+</div>
+  </div>
   <div v-else>
     <audio id="radio" class="hidden" preload="none" ref="music" />
     <currently-playing v-if="currentStation" :station="currentStation" v-on:stationEvent="handleStationEvent" v-on:volume="handleVolume" :volume="volume"/>
@@ -151,3 +161,73 @@ export default {
   </div>
   <music-bar />
 </template>
+<style scoped>
+  .loadingContainer {
+    z-index: 100;
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  .lds-ellipsis {
+    position: relative;
+    margin: auto;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ellipsis div {
+    position: absolute;
+    top: 33px;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: #fff;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+  .lds-ellipsis div:nth-child(1) {
+    left: 8px;
+    animation: lds-ellipsis1 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(2) {
+    left: 8px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(3) {
+    left: 32px;
+    animation: lds-ellipsis2 0.6s infinite;
+  }
+  .lds-ellipsis div:nth-child(4) {
+    left: 56px;
+    animation: lds-ellipsis3 0.6s infinite;
+  }
+  @keyframes lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+  @keyframes lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(24px, 0);
+    }
+  }
+
+</style>
